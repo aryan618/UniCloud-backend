85fb0342b7c0cb535664e9933e35a14e95964af7
shre-yansh <itnakim161@gmail.com>
2024-01-27 19:49:05 +0530

    formatting

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
 const { Api, TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
+
+require('dotenv').config();
+
 const express = require('express');
+
 const app = express();
 const port = 3000;
-require('dotenv').config();
+
 const stringsession = new StringSession(""); // You should put your string session here
-let apiId=process.env.apiId;
-let apiHash = process.env.apiHash;
+
+let apiId= 928403;
+let apiHash = "4a546388b91f6f815c4a6adbbc30d574";
+
+let clients = {};
 
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.status(200).send(`Wow the server is running da`);
-})
-// const client = new TelegramClient(session, apiId, apiHash, {});
 
-// (async function run() {
-//   await client.connect(); // This assumes you have already authenticated with .start()
-//     console.log(client);
-//   const result = await client.invoke(
-//     new Api.auth.SendCode({
-//       phoneNumber: "+917004232453",
-//       apiId: apiId,
-//       apiHash: apiHash,
-//       settings: new Api.CodeSettings({
-//         allowFlashcall: true,
-//         currentNumber: true,
-//         allowAppHash: true,
-//         allowMissedCall: true,
-//         logoutTokens: [Buffer.from("arbitrary data here")],
-//       }),
-//     })
-//   );
-//   console.log(result); // prints the result
-// })();
-let client=null;
-app.post('/getOtp',async (req,res)=>{
-    const {phoneNumber} = req.body;
+app.get('/',(req,res)=>{
+    res.status(200).send(`Server Running`);
+})
+
+app.post('/send_otp',async (req,res)=>{
+    const {phone_number} = req.body;
     try{
-         client = new TelegramClient(stringsession, apiId, apiHash, {});
-        //client1={...client};
+        let client = new TelegramClient(stringsession, apiId, apiHash, {});
+        
         await client.connect();
         const result = await client.invoke(
                 new Api.auth.SendCode({
-                 // phoneNumber: "+917004232453",
-                 phoneNumber:phoneNumber,
+                 phoneNumber:phone_number,
                   apiId: apiId,
                   apiHash: apiHash,
                   settings: new Api.CodeSettings({
@@ -55,42 +41,55 @@ app.post('/getOtp',async (req,res)=>{
                   }),
                 })
               );
-        res.status(200).json({message:`Sent otp successfully`,result:result});
+        clients[phone_number]=client;
+        await client.disconnect();
+        res.status(200).json({message:`Sent otp successfully`,phone_code_hash:result.phoneCodeHash, phone_number:result.phoneNumber});
         
     } catch (error) {
-        console.error('Login error:', error);
+      if (error.phone_code_expired){
+        res.status(429).json({error:`OTP expired`});
+      }
+      else if (error.phone_number_invalid){
+        res.status(401).json({error:`Internal servor error`});
+      }
         res.status(500).json({ error: 'Internal Server Error' });
       }
 })
 
-app.post('/login',async (req,res)=>{
-    const {otp,phoneHash,phoneNumber}= req.body;
+app.post('/sign_in',async (req,res)=>{
+    const {phone_code_hash,phone_number, code}= req.body;
     try{
-        //const client={...client1};
-        console.log(client);
+        let client = clients[phone_number];
         await client.connect();
         const result = await client.invoke(
             new Api.auth.SignIn({
-              phoneNumber: phoneNumber,
-              phoneCodeHash: phoneHash,
-              phoneCode: otp,
+              phoneNumber: phone_number,
+              phoneCodeHash: phone_code_hash,
+              phoneCode: code,
             })
           );
         const sessionString=client.session.save();
         console.log(sessionString);
         res.status(200).json({message:`Connected successfully`,session:sessionString});
+        await client.disconnect();
+        delete clients[phoneNumber];
 
     } catch(error){
+      
+      console.log(error);
         res.status(500).json({error:`Internal servor error`});
     }
 })
 
-app.get('/welcome-message',async (req,res)=>{
+app.get('/send',async (req,res)=>{
     try{
-        await client.connect();
+      let session = req.headers['auth'];
+      let client = new TelegramClient(new StringSession(session), apiId, apiHash, {});
+      await client.connect();
         await client.sendMessage('me',{message:`Hello bete`});
         res.status(200).json({message:`Welcome message sent successfully`});
     } catch(error){
+      console.log(error);
         res.status(500).json({error:`Internal servor error in sending message`});
     }
 
